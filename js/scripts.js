24e4dfc3abe52c8ce8486967de792a4fbd978184
marblo <marcinblochowiak@wp.pl>
2017-04-26 20:46:15 +0200

    first step

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var triangle1Area = getTriangleArea(10,15)
+triangle2Area = getTriangleArea(12,6)
+triangle3Area = getTriangleArea(8,4)
+
+function getTriangleArea(a,h) {
+	var wynik = a*h/2;
+	if 
+		(a <=0 || h <= 0) {
+			console.log('Złe dane');
+		}
+	else{
+		console.log(wynik);
+	}	
+}
+
+
