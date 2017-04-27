7434ad9130128b106b07d3ada4fcf74066473a2e
marblo <marcinblochowiak@wp.pl>
2017-04-27 21:48:13 +0200

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
-var triangle1Area = getTriangleArea(10,15)
-triangle2Area = getTriangleArea(12,6)
-triangle3Area = getTriangleArea(8,4)
+var triangle1Area = getTriangleArea(10,15),
+triangle2Area = getTriangleArea(12,6),
+triangle3Area = getTriangleArea(8,4);
 
 function getTriangleArea(a,h) {
-	var wynik = a*h/2;
-	if 
-		(a <=0 || h <= 0) {
-			console.log('Złe dane');
-		}
-	else{
-		console.log(wynik);
-	}	
+var score = a*h/2;
+if 
+	(a <=0 || h <= 0) {
+		console.log('Złe dane');
+} else {
+	console.log(score);
+}	
 }
 
 
